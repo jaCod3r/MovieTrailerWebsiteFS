df9b9c65a1c55a163644004831de3cb0e3e1e8e9
Juber Ahmed <a.juber@hotmail.com>
2016-03-21 15:46:51 -0400

    Nothing Special - Just saving code

---
+++ entertainment_center.py
@@ -0,0 +1,10 @@
+import media
+
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and and his toys that come to life",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "http://www.youtube.com/watch?v=vwyZH85NQC4")
+
+
+print(toy_story.storyline)
