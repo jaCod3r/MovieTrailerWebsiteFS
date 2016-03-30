509b0e0c01526aabd13b2ef7673b3afc0efea4f2
Juber Ahmed <a.juber@hotmail.com>
2016-03-30 16:31:44 -0400

    Added comments to code

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,6 +2,7 @@ import media
 import fresh_tomatoes
 
 
+#Generates formated movie trailers of different movies
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and and his toys that come to life",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
--- media.py
+++ media.py
@@ -1,13 +1,18 @@
 import webbrowser
 
 
+""" This class stores movie information. """
 class Movie:
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
+        """ This is the constructor that takes multiple perameters
+            the title, the story line, an image, and a link to
+            youtube tailer. Able to create muliple instances."""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """ When method is called a youtube trailer opens"""
         webbrowser.open(self.trailer_youtube_url)
